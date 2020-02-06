6c47f825f0d4b35b904616bb11a1df3919a7248a
James Lenahan <james.k.lenahan@gmail.com>
2020-02-06 17:53:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   get '/' do
     "Hello, World!"
+  end
   get 'name' do
     "My name is_"
+  end
   get 'hometown' do
     "My hometown is_"
+  end
   get 'favorite-song' do
     "My favorite song is_"
   end
